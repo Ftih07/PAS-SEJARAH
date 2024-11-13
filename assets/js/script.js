93f5bcd287faf61a76746215917392d55b77a8a7
Ftih07 <naufalfathi37@gmail.com>
2024-11-13 20:49:52 +0700

    quiz

---
+++ assets/js/script.js
@@ -0,0 +1,518 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Apa yang menjadi pemicu utama terjadinya kerusuhan di Indonesia pada tahun 1998?",
+      answer: "Krisis ekonomi",
+      options: [
+        "Krisis ekonomi",
+        "Pemilu yang curang",
+        "Konflik agama",
+        "Perubahan sistem pemerintahan",
+      ],
+    },
+    {
+      numb: 2,
+      question: "Siapa presiden Indonesia saat kerusuhan 1998 terjadi?",
+      answer: "Soeharto",
+      options: [
+        "Habibie",
+        "Soekarno",
+        "Soeharto",
+        "Megawati",
+      ],
+    },
+    {
+      numb: 3,
+      question: "Apa yang dimaksud dengan Reformasi 1998 dalam konteks sejarah Indonesia?",
+      answer: "Proses perubahan politik untuk mengakhiri Orde Baru",
+      options: [
+        "Perubahan dalam struktur ekonomi",
+        "Penggantian presiden tanpa kekerasan",
+        "Proses perubahan politik untuk mengakhiri Orde Baru",
+        "Penurunan tarif pajak",
+      ],
+    },
+    {
+      numb: 4,
+      question: "Bagaimana kondisi ekonomi Indonesia menjelang kerusuhan 1998?",
+      answer: "Mengalami krisis moneter",
+      options: [
+        "Stabil",
+        "Meningkat pesat",
+        "Mengalami krisis moneter",
+        "Terus mengalami surplus",
+      ],
+    },
+    {
+      numb: 5,
+      question: "Apa yang menjadi alasan utama masyarakat Indonesia merasa kecewa terhadap pemerintahan Orde Baru?",
+      answer: "Korupsi yang merajalela",
+      options: [
+        "Korupsi yang merajalela",
+        "Pemimpin otoriter",
+        "Ketimpangan ekonomi",
+        "Kurangnya kebebasan berpendapat",
+      ],
+    },
+    {
+      numb: 6,
+      question: "Apa yang diminta oleh mahasiswa dalam aksi protes mereka pada 1998?",
+      answer: "Reformasi dan pengunduran diri Soeharto",
+      options: [
+        "Peningkatan kesejahteraan",
+        "Reformasi dan pengunduran diri Soeharto",
+        "Kebebasan berpendapat",
+        "Pemilu yang lebih adil",
+      ],
+    },
+    {
+      numb: 7,
+      question: "Apa yang terjadi pada 21 Mei 1998?",
+      answer: "Soeharto mengundurkan diri dari jabatan presiden",
+      options: [
+        "Kekerasan semakin meningkat",
+        "Soeharto mengundurkan diri dari jabatan presiden",
+        "Presiden Habibie dilantik",
+        "Mahasiswa menguasai gedung-gedung pemerintahan",
+      ],
+    },
+    {
+      numb: 8,
+      question: "Bagaimana kerusuhan 1998 berdampak terhadap masyarakat Indonesia?",
+      answer: "Penjarahan, kekerasan, dan kerusakan besar-besaran",
+      options: [
+        "Meningkatkan semangat persatuan",
+        "Penjarahan, kekerasan, dan kerusakan besar-besaran",
+        "Perubahan ekonomi yang cepat",
+        "Reformasi politik yang damai",
+      ],
+    },
+    {
+      numb: 9,
+      question: "Siapa yang menggantikan Soeharto setelah pengunduran dirinya?",
+      answer: "B.J. Habibie",
+      options: [
+        "Megawati",
+        "B.J. Habibie",
+        "Prabowo Subianto",
+        "Amien Rais",
+      ],
+    },
+    {
+      numb: 10,
+      question: "Apa yang dimaksud dengan 'Reformasi' yang terjadi setelah 1998?",
+      answer: "Perubahan besar dalam sistem politik, ekonomi, dan sosial",
+      options: [
+        "Perubahan besar dalam sistem politik, ekonomi, dan sosial",
+        "Peningkatan pembangunan ekonomi",
+        "Mengakhiri masa jabatan presiden",
+        "Penurunan harga barang-barang pokok",
+      ],
+    },
+    {
+      numb: 11,
+      question: "Apa yang menyebabkan kerusuhan 1998 terutama di Jakarta?",
+      answer: "Krisis ekonomi dan kebijakan pemerintah yang tidak popular",
+      options: [
+        "Protes terhadap kebijakan luar negeri",
+        "Krisis ekonomi dan kebijakan pemerintah yang tidak popular",
+        "Peningkatan harga barang",
+        "Konflik sosial antar etnis",
+      ],
+    },
+    {
+      numb: 12,
+      question: "Siapa yang memimpin aksi mahasiswa pada tahun 1998?",
+      answer: "Mahasiswa dari berbagai universitas",
+      options: [
+        "Amien Rais",
+        "Habibie",
+        "Mahasiswa dari berbagai universitas",
+        "Prabowo Subianto",
+      ],
+    },
+    {
+      numb: 13,
+      question: "Bagaimana pemerintah Indonesia merespon kerusuhan 1998?",
+      answer: "Dengan menerapkan keadaan darurat dan menurunkan aparat militer",
+      options: [
+        "Dengan menyelenggarakan dialog terbuka",
+        "Dengan menerapkan keadaan darurat dan menurunkan aparat militer",
+        "Dengan memecat beberapa pejabat tinggi",
+        "Dengan membentuk komite investigasi",
+      ],
+    },
+    {
+      numb: 14,
+      question: "Apa yang terjadi dengan pemerintahan Orde Baru setelah kerusuhan 1998?",
+      answer: "Orde Baru berakhir dan sistem pemerintahan berubah",
+      options: [
+        "Pemerintahan Orde Baru menguat",
+        "Orde Baru berakhir dan sistem pemerintahan berubah",
+        "Pemerintahan tetap stabil",
+        "Tidak ada perubahan signifikan",
+      ],
+    },
+    {
+      numb: 15,
+      question: "Siapa yang bertanggung jawab atas peristiwa kerusuhan 1998 menurut beberapa pihak?",
+      answer: "Pemerintah dan aparat keamanan",
+      options: [
+        "Mahasiswa",
+        "Pemerintah dan aparat keamanan",
+        "Pengusaha",
+        "Masyarakat umum",
+      ],
+    },
+    {
+      numb: 16,
+      question: "Bagaimana dampak sosial kerusuhan 1998 terhadap masyarakat?",
+      answer: "Meningkatnya ketegangan antar kelompok sosial dan etnis",
+      options: [
+        "Meningkatnya ketegangan antar kelompok sosial dan etnis",
+        "Peningkatan rasa persatuan nasional",
+        "Menguatnya ekonomi domestik",
+        "Menurunnya jumlah pengangguran",
+      ],
+    },
+    {
+      numb: 17,
+      question: "Bagaimana media berperan dalam kerusuhan 1998?",
+      answer: "Memberikan pemberitaan yang memicu ketegangan dan kekerasan",
+      options: [
+        "Memberikan informasi yang menenangkan",
+        "Memberikan pemberitaan yang memicu ketegangan dan kekerasan",
+        "Tidak melaporkan kejadian tersebut",
+        "Menjadi mediator antara pihak pemerintah dan masyarakat",
+      ],
+    },
+    {
+      numb: 18,
+      question: "Apa akibat langsung dari pengunduran diri Soeharto?",
+      answer: "Perubahan signifikan dalam sistem politik Indonesia",
+      options: [
+        "Pemerintahan tetap stabil",
+        "Perubahan signifikan dalam sistem politik Indonesia",
+        "Peningkatan hubungan internasional",
+        "Pengurangan kemiskinan secara drastis",
+      ],
+    },
+    {
+      numb: 19,
+      question: "Apa yang dimaksud dengan 'Orde Baru'?",
+      answer: "Periode pemerintahan yang dimulai dengan Soeharto pada 1966",
+      options: [
+        "Periode pemerintahan yang dimulai dengan Soeharto pada 1966",
+        "Periode demokrasi yang baru dimulai",
+        "Periode reformasi politik",
+        "Pemerintahan yang dipimpin oleh Soekarno",
+      ],
+    },
+    {
+      numb: 20,
+      question: "Apa yang menjadi fokus utama pasca-kerusuhan 1998?",
+      answer: "Reformasi politik dan perbaikan ekonomi",
+      options: [
+        "Pengurangan utang luar negeri",
+        "Reformasi politik dan perbaikan ekonomi",
+        "Pemulihan hubungan internasional",
+        "Peningkatan investasi asing",
+      ],
+    },
+];
+  
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+// if startQuiz button clicked
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo"); //show info box
+};
+
+// if exitQuiz button clicked
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //hide info box
+};
+
+// if continueQuiz button clicked
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //hide info box
+  quiz_box.classList.add("activeQuiz"); //show quiz box
+  showQuetions(0); //calling showQestions function
+  queCounter(1); //passing 1 parameter to queCounter
+  startTimer(15); //calling startTimer function
+  startTimerLine(0); //calling startTimerLine function
+};
+
+let timeValue = 15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = () => {
+  quiz_box.classList.add("activeQuiz"); //show quiz box
+  result_box.classList.remove("activeResult"); //hide result box
+  timeValue = 15;
+  que_count = 0;
+  que_numb = 1;
+  userScore = 0;
+  widthValue = 0;
+  showQuetions(que_count); 
+  queCounter(que_numb); 
+  clearInterval(counter); 
+  clearInterval(counterLine); 
+  startTimer(timeValue); 
+  startTimerLine(widthValue); 
+  timeText.textContent = "Time Left"; 
+  next_btn.classList.remove("show"); 
+};
+
+// if quitQuiz button clicked
+quit_quiz.onclick = () => {
+  window.location.reload(); 
+};
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// if Next Que button clicked
+next_btn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    //if question count is less than total question length
+    que_count++; //increment the que_count value
+    que_numb++; //increment the que_numb value
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    startTimer(timeValue); //calling startTimer function
+    startTimerLine(widthValue); //calling startTimerLine function
+    timeText.textContent = "Time Left"; //change the timeText to Time Left
+    next_btn.classList.remove("show"); //hide the next button
+  } else {
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    showResult(); //calling showResult function
+  }
+};
+
+// getting questions and options from array
+function showQuetions(index) {
+  const que_text = document.querySelector(".que_text");
+
+  //creating a new span and div tag for question and option and passing the value using array index
+  let que_tag =
+    "<span>" +
+    questions[index].numb +
+    ". " +
+    questions[index].question +
+    "</span>";
+  let option_tag =
+    '<div class="option"><span>' +
+    questions[index].options[0] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[1] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[2] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[3] +
+    "</span></div>";
+  que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+  option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+
+  const option = option_list.querySelectorAll(".option");
+
+  // set onclick attribute to all available options
+  for (i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//if user clicked on option
+function optionSelected(answer) {
+  clearInterval(counter); //clear counter
+  clearInterval(counterLine); //clear counterLine
+  let userAns = answer.textContent; //getting user selected option
+  let correcAns = questions[que_count].answer; //getting correct answer from array
+  const allOptions = option_list.children.length; //getting all option items
+
+  if (userAns == correcAns) {
+    //if user selected option is equal to array's correct answer
+    userScore += 1; //upgrading score value with 1
+    answer.classList.add("correct"); //adding green color to correct selected option
+    answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+    console.log("Correct Answer");
+    console.log("Your correct answers = " + userScore);
+  } else {
+    answer.classList.add("incorrect"); //adding red color to correct selected option
+    answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+    console.log("Wrong Answer");
+
+    for (i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correcAns) {
+        //if there is an option which is matched to an array answer
+        option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+        console.log("Auto selected correct answer.");
+      }
+    }
+  }
+  for (i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+  }
+  next_btn.classList.add("show"); //show the next button if user selected any option
+}
+
+function showResult() {
+  info_box.classList.remove("activeInfo"); //hide info box
+  quiz_box.classList.remove("activeQuiz"); //hide quiz box
+  result_box.classList.add("activeResult"); //show result box
+  const scoreText = result_box.querySelector(".score_text");
+  if (userScore > 3) {
+    // if user scored more than 3
+    //creating a new span tag and passing the user score number and total question number
+    let scoreTag =
+      "<span>and congrats! 🎉, You got <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
+  } else if (userScore > 1) {
+    // if user scored more than 1
+    let scoreTag =
+      "<span>and nice 😎, You got <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag;
+  } else {
+    // if user scored less than 1
+    let scoreTag =
+      "<span>and sorry 😐, You got only <p>" +
+      userScore +
+      "</p> out of <p>" +
+      questions.length +
+      "</p></span>";
+    scoreText.innerHTML = scoreTag;
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time; //changing the value of timeCount with time value
+    time--; //decrement the time value
+    if (time < 9) {
+      //if timer is less than 9
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero; //add a 0 before time value
+    }
+    if (time < 0) {
+      //if timer is less than 0
+      clearInterval(counter); //clear counter
+      timeText.textContent = "Time Off"; //change the time text to time off
+      const allOptions = option_list.children.length; //getting all option items
+      let correcAns = questions[que_count].answer; //getting correct answer from array
+      for (i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == correcAns) {
+          //if there is an option which is matched to an array answer
+          option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+          option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+          console.log("Time Off: Auto selected correct answer.");
+        }
+      }
+      for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+      }
+      next_btn.classList.add("show"); //show the next button if user selected any option
+    }
+  }
+}
+
+function startTimerLine(time) {
+  counterLine = setInterval(timer, 29);
+  function timer() {
+    time += 1; //upgrading time value with 1
+    time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+    if (time > 549) {
+      //if time value is greater than 549
+      clearInterval(counterLine); //clear counterLine
+    }
+  }
+}
+
+function queCounter(index) {
+  let totalQueCounTag =
+    "<span><p>" +
+    index +
+    "</p> of <p>" +
+    questions.length +
+    "</p> Questions</span>";
+  bottom_ques_counter.innerHTML = totalQueCounTag;
+}
+
+function startTimer(time) {
+    const beepAudio = new Audio('meme.mp3');  // Specify your beep sound file here
+    counter = setInterval(timer, 1000);
+  
+    function timer() {
+      timeCount.textContent = time; // Updating the time displayed
+      time--; // Decrement time
+  
+      if (time < 9) {
+        // Adding leading zero when the time is less than 9
+        let addZero = timeCount.textContent;
+        timeCount.textContent = "0" + addZero;
+      }
+  
+      if (time === 5) {  // Play sound 5 seconds before time runs out
+        beepAudio.play();
+      }
+  
+      if (time < 0) {
+        clearInterval(counter); // Stop the timer
+        timeText.textContent = "Time Off"; // Notify time is off
+        const allOptions = option_list.children.length; // Get all options
+        let correcAns = questions[que_count].answer; // Get the correct answer
+  
+        // Loop to highlight the correct answer if time runs out
+        for (i = 0; i < allOptions; i++) {
+          if (option_list.children[i].textContent == correcAns) {
+            option_list.children[i].setAttribute("class", "option correct");
+            option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+          }
+        }
+  
+        // Disable all options once time is up
+        for (i = 0; i < allOptions; i++) {
+          option_list.children[i].classList.add("disabled");
+        }
+        next_btn.classList.add("show"); // Show the next button
+      }
+    }
+  }
+  
